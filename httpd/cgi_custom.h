8a5b2eabfdf697ce9a95ee53009a61ac41d2f4df
tjjh89017 <tjjh89017@hotmail.com>
2013-07-28 23:45:10 +0800

    update

--- httpd/cgi_custom.h
+++ httpd/cgi_custom.h
@@ -3,31 +3,13 @@
 
 #include "eserv/cgi.h"
 
-extern int cgi_page_sum(ExHttp *pHttp);
-extern int cgi_page_txt(ExHttp *pHttp);
 extern int cgi_page_login(ExHttp *pHttp);
-extern int cgi_page_gallery(ExHttp *pHttp);
 
 /* customized page handler declare here */
 cgi_page cgi_pages[] = {
 	{
-		.name = "sum.cgi",
-		.callback = cgi_page_sum,
-	},
-
-	{
-		.name = "txt.cgi",
-		.callback = cgi_page_txt,
-	},
-
-	{
 		.name = "login.cgi",
 		.callback = cgi_page_login,
-	},
-
-	{
-		.name = "gallery.cgi",
-		.callback = cgi_page_gallery,
 	},
 };
 
